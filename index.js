71a84ad993a39436652dd5894ecfe4e1e6cc4c29
Mohit Dhatrak <91209576+mohitdhatrak@users.noreply.github.com>
2022-09-24 01:53:52 +0000

    code refactor, moved res to try

--- index.js
+++ index.js
@@ -4,20 +4,18 @@ const axios = require('axios').default;
 
 const app = express();
 
-
 router.get("/", async (req, res) => {
   let response;
   const apiURL = "https://breakingbadapi.com/api/characters";
 
   try {
     response = await axios.get(apiURL);
+    res.json(response.data);
     // console.log(response.data);
   }
   catch (err) {
     res.send("Some error has occured, please try again later!");
   }
-
-  res.json(response.data);
 });
 
 app.use(router);
