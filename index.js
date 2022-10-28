63b49728b45af6ba39efeb2024a93978636feda4
Mohit Dhatrak <91209576+mohitdhatrak@users.noreply.github.com>
2022-10-28 18:20:23 +0000

    added bonus task

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ router.get("/", async (req, res) => {
   }
 });
 
+// bonus task
+router.get("/bonus", async (req, res) => {
+  let response;
+  const bonusURL = "https://breakingbadapi.com/api/characters?category=Better+Call+Saul"
+
+  try {
+    response = await axios.get(bonusURL);
+    res.json(response.data);
+    // console.log(response.data);
+  }
+  catch (err) {
+    res.send("Some error has occured, please try again later!");
+  }
+});
+
 app.use(router);
 
 app.listen(3000);
